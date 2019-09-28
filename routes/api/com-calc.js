840e41035b01d13d81b3ec501e1da416c4e80818
Thuan Tang <thuantang@gmail.com>
2019-09-27 19:00:00 -0700

    Reorganized application code vs assignment code and updated some inconsistencies

--- routes/api/com-calc.js
+++ routes/api/com-calc.js
@@ -1,4 +1,3 @@
-const cheerio = require('cheerio'); // scraper
 const express = require('express');
 const router = express.Router();
 
@@ -33,11 +32,6 @@ router.post('/', (req, res) => {
     homeCost: req.body.homeCost,
     workHours: req.body.workHours
   };
-
-  // mockup for scraping webpages for data
-  html_data = '<html op="news"><head><title>Mockup data for title</title></head><body>Mockup data for body</body></html>';
-  const $ = cheerio.load(html_data);
-  console.log('This is the title from raw html:', $('title').text());
 
   console.log('\nServer received post request at /api/com-calc');
   console.log('data:', state);
--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 const express = require('express'); // server connection
 const mongoose = require('mongoose'); // mongodb connection
 const bodyParser = require('body-parser'); // parses post requests
-const moment = require('moment'); // improves time and date functionality
-const path = require('path');
-const axios = require('axios');
-const comCalc = require('./routes/api/com-calc'); // example api
-const users = require('./routes/api/users');
-// we currently do not have an api key yet, and we probably shouldn't hard code itanyway
-const googleMapsClient = require('@google/maps').createClient({
- key: 'your API key here'
-});
+const path = require('path'); // server path
+const comCalc = require('./routes/api/com-calc'); // commuter calculator
 
 // mongodb connection
 const db = require('./config/keys').mongoURI;
@@ -21,32 +14,51 @@ mongoose
 // server app
 const app = express();
 
-// bodyParser middleware
+// bodyParser middleware - require to parse post parameter requests
 app.use(bodyParser.json());
 
-// use routes
+// commuter calculator api
 app.use('/api/com-calc', comCalc);
-
-// team members api for assignment 3
-app.use('/api/users', users);
-
-var date = new Date();
-var time = moment(date).format();
 
 // serve static files from the build folder
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // server the home page from the root url.
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port} at ${time}...`);
-
-  // Axios example of HTTP Calls
-  axios
-    .get('http://google.com')
-    .then(res => console.log('Data response from google', res.data));
 });
+
+/*******************************************************************************
+ * Assignment code blow
+ ******************************************************************************/
+const axios = require('axios');              // ajax calls
+const cheerio = require('cheerio');          // web scraper
+const moment = require('moment');            // datetime
+const users = require('./routes/api/users'); // users api
+// we currently do not have an api key yet, and we probably shouldn't hard code itanyway
+const googleMapsClient = require('@google/maps').createClient({
+  key: 'your API key here'
+});
+
+// http users api for assignment 3
+app.use('/api/users', users); 
+
+// Cheerio api
+// mockup for scraping webpages for data
+html_data = '<html op="news"><head><title>Mockup data for title</title></head><body>Mockup data for body</body></html>';
+const $ = cheerio.load(html_data);
+console.log('This is the title from raw html:', $('title').text());
+
+// Moment api
+var date = new Date();
+var time = moment(date).format();
+
+// Axios example of HTTP Calls
+axios
+  .get('http://google.com')
+  .then(res => console.log('Data response from google', res.data.slice(0, 15)));
