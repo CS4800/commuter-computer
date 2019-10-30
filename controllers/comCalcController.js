0274064046590c782aae50e2dc7b96cfb8da7b73
Thuan Tang <thuantang@gmail.com>
2019-10-30 08:02:29 -0700

    Added departure time to calculation

--- controllers/comCalcController.js
+++ controllers/comCalcController.js
@@ -1,5 +1,6 @@
 const calculate = require('../lib/calculate');
 const { distanceMatrix } = require('../lib/googleAPI');
+const util = require('../lib/util');
 let GasPrice = require('../models/gasPriceModel');
 let RentPrice = require('../models/rentPriceModel');
 
@@ -40,6 +41,11 @@ async function post(req, res) {
   });
   state.gas = gas.prices;
 
+  // get next Wednesday and reset startTime / endTime to time since epoch
+  let wed = util.getNextDay(3);
+  state.startTime = util.getTimeSinceEpoch(wed, state.startTime);
+  state.endTime = util.getTimeSinceEpoch(wed, state.endTime);
+
   // get distance and duration from home address to remot address
   const {
     matrices,
@@ -47,10 +53,12 @@ async function post(req, res) {
     destination_addresses
   } = await distanceMatrix(
     state.homeAddresses[0] + '|' + state.homeAddresses[1],
-    state.remoteAddr
+    state.remoteAddr,
+    Math.round(state.startTime / 1000) // millisec to sec
   );
   state.matrices = matrices;
 
+  // reset home addresses to full addresses from api
   state.homeAddresses[0] = origin_addresses[0] ? origin_addresses[0] : '';
   state.homeAddresses[1] = origin_addresses[1] ? origin_addresses[1] : '';
 
