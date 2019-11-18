1eb27dcddd3b1499c6f54f05fff0fb0f1fe82614
youngjun114 <youngjun_92@hotmail.com>
2019-11-18 10:27:27 -0800

    Add logo and favicon to client page

--- client/src/components/layout/AppNavbar.js
+++ client/src/components/layout/AppNavbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import logo from '../../logo.svg';
 import { NavLink as RouterNavLInk } from 'react-router-dom';
 import {
   Collapse,
@@ -29,6 +30,8 @@ class AppNavbar extends Component {
         <Navbar color='dark' dark expand='sm' className='mb-5'>
           <Container>
             <NavbarBrand tag={RouterNavLInk} to='/'>
+              <img src={logo} className='App-logo' alt='logo' width='40' />
+              <span className='mr-2'>{'  '}</span>
               {this.props.title}
             </NavbarBrand>
             <NavbarToggler onClick={this.toggle} />
